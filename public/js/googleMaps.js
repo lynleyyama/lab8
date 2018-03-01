f36c5483d78178ee129bd5b7270206349b3bc2ca
Lynley Yamaguchi <lynleyyama@yahoo.com>
2018-03-01 10:47:54 -0800

    lab 8

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,14 @@
-function initMap() {
-	// add your code here
+function initMap(){
+	var ucsd_ltlng = {lat:32.885002, lng:-117.241271};
+
+	var map = new google.maps.Map(document.getElementById('map'),{
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
 }
